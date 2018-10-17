ce4dcdd69d152cf85eb5722453dd8e1cf7dca4f2
jwilly0508 <jwilli0508@gmail.com>
2018-10-16 21:35:51 -0500

    WebFlux dummy

--- src/main/java/com/studio/reactiveapplication/springbootlearn/controller/GraphQLController.java
+++ src/main/java/com/studio/reactiveapplication/springbootlearn/controller/GraphQLController.java
@@ -11,11 +11,15 @@ import io.leangen.graphql.metadata.strategy.query.AnnotatedResolverBuilder;
 import io.leangen.graphql.metadata.strategy.value.jackson.JacksonValueMapperFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
 import javax.servlet.http.HttpServletRequest;
 
+import java.time.Duration;
 import java.util.Map;
 
 @RestController
@@ -46,4 +50,11 @@ public class GraphQLController {
         return executionResult.toSpecification();
 
     }
+
+    @GetMapping(value = "/graphiql/numeros",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getNumbers(){
+            return Flux.range(1,30).
+                    delayElements(Duration.ofSeconds(1)).map(n->n.toString());
+    }
+
 }
